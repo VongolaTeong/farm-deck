c83050242787fa9ac23c29020081db324659b842
teong <0522teongkarchun@gmail.com>
2025-08-01 19:43:04 +0800

    feat: add melon seed selector

--- crop_plot.gd
+++ crop_plot.gd
@@ -9,6 +9,8 @@ var tile_type: Dictionary = {}
 
 const TILE_TYPES: Array = [0, 1]
 
+var selected_seed = TILE_TYPES[0]
+
 func _ready() -> void:
 	# Connect the input_event signal from this Area2D to our handling function
 	input_event.connect(_on_input_event)
@@ -28,7 +30,7 @@ func hide_crops() -> void:
 	
 func advance_tile_growth(tile_pos: Vector2i) -> void:
 	var stage = tile_growth.get(tile_pos, 0)
-	var type = tile_type.get(tile_pos, TILE_TYPES[1])
+	var type = tile_type.get(tile_pos, selected_seed)
 	if stage == 4:
 		print("Tile", tile_pos, "is fully grown")
 		return
@@ -40,3 +42,7 @@ func advance_tile_growth(tile_pos: Vector2i) -> void:
 	stage += 1
 	tile_growth[tile_pos] = stage
 	crop.set_cell(tile_pos, 0, Vector2i(stage, type))  # Assumes tile index 0 is your crop tile
+
+
+func _on_melon_seed_button_pressed() -> void:
+	selected_seed = TILE_TYPES[1]
