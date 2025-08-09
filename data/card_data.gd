aa0d7055ea40247db23f200f133e24f8c4d87f71
teong <0522teongkarchun@gmail.com>
2025-08-09 09:23:15 +0800

    feat: add sample card

---
+++ data/card_data.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name CardData
+
+@export var name: String
+@export var description: String
+@export var icon: Texture
+@export var cost: int
+@export var type: String # e.g., "Attack", "Heal", "Defend"
