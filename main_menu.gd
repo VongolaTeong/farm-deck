2cd8f12763dc845ac57cda8400c3fe0dad3f7b76
teong <0522teongkarchun@gmail.com>
2025-08-29 19:22:47 +0800

    fix: hide farm signal incorrectly connected

--- main_menu.gd
+++ main_menu.gd
@@ -12,11 +12,9 @@ func _on_card_button_pressed() -> void:
 	main_menu_scene.hide()
 	deck_builder_scene.show()
 
-
 func _on_deck_builder_hide_deck_builder() -> void:
 	deck_builder_scene.hide()
 	main_menu_scene.show()
-
 
 func _on_farm_hide_farm() -> void:
 	farm_scene.hide()
